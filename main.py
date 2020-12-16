2aba16b9b680fdb780c88401e25088b259048141
volkir31 <egor_mixajlichenko@mail.ru>
2020-12-16 14:15:51 +0300

    refactoring

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, json
 
 app = Flask(__name__)
 
 translite_dict = {
+
     'А': 'A', 'Б': 'B', 'В': 'V', 'Г': 'G', 'Д': 'D', 'Е': 'E', 'Ё': 'E', 'Ж': 'ZH', 'З': 'Z', 'И': 'I', 'Й': 'I',
     'К': 'K', 'Л': 'L', 'М': 'M', 'Н': 'N', 'О': 'O', 'П': 'P', 'Р': 'R', 'С': 'S', 'Т': 'T', 'У': 'U', 'Ф': 'F',
     'Х': 'KH', 'Ц': 'TC', 'Ч': 'CH', 'Ш': 'SH', 'Щ': 'SHCH', 'Ы': 'Y', 'Э': 'E', 'Ю': 'IU', 'Я': 'IA',
@@ -21,10 +22,15 @@ def translite(word):
 
 @app.route('/', methods=['post', 'get'])
 def main():
+    return render_template('index.html')
+
+
+@app.route('/translite', methods=['GET', 'POST'])
+def get_len():
     word = ''
     if request.method == 'POST':
         word = request.form.get('word')
-    return render_template('index.html', message=translite(word))
+    return translite(word)
 
 
 @app.route('/api')
